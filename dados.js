a8fa0938241e73ac6a35ff0a64cbfb334a050763
SamuelDinizTenorio <samueldinizcti@outlook.com>
2024-09-07 19:16:24 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,26 +1,30 @@
 let rayssa = {
     titulo : "Rayssa Leal",
     descricao : "Rayssa Leal: A Fadinha do Skate. Rayssa Leal, carinhosamente apelidada de Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos 16 anos, ela já coleciona diversos títulos, incluindo medalhas olímpicas de prata e bronze, além de ser campeã pan-americana e mundial.",
-    link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
-}
+    link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags : "Skate Skatista Fadinha Fada"
+};
 
 let rebeca = {
     titulo : "Rebeca Andrade",
     descricao : "Rebeca Andrade: A Maior Medalhista Olímpica Brasileira. Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros com sua determinação e talento. Bicampeã olímpica e a maior medalhista da história do Brasil nos Jogos Olímpicos, Rebeca se tornou um símbolo nacional do esporte.",
-    link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
-}
+    link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags : "Gisnástica Ginastica Ginasta"
+};
 
 let beatriz = {
     titulo: "Beatriz Sousa",
     descricao: "Beatriz Souza: Um Novo Brilho no Judô Brasileiro. Beatriz Souza, a Bia, é uma judoca brasileira que vem conquistando o mundo com suas performances excepcionais. A atleta, que já é considerada um dos principais nomes do judô feminino brasileiro, coleciona medalhas em diversas competições, como o Mundial e o Pan-Americano. Sua força, técnica e determinação a levaram ao topo do pódio em diversas oportunidades, tornando-a um grande orgulho para o Brasil.",
-    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    tags : "Judô Judo Judoca Judoka"
 };
 
 let tatiana = {
     titulo : "Tatiana Weston-webb",
     descricao : "Tatiana Weston-Webb: Uma força da natureza nas ondas Tatiana Guimarães Weston-Webb é uma surfista profissional que conquistou o mundo com sua habilidade e determinação. Nascida no Brasil e criada no Havaí, ela representa o Brasil em competições internacionais e já conquistou diversos títulos importantes, como etapas do World Surf League Championship Tour e medalhas em Jogos Olímpicos e Pan-Americanos.",
-    link : "https://pt.wikipedia.org/wiki/Tatiana_Weston-Webb"
-}
+    link : "https://pt.wikipedia.org/wiki/Tatiana_Weston-Webb",
+    tags : "Surf Surfista"
+};
 
 let dados = [
     rayssa, 
