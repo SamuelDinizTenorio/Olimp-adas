ab2c5a2a59371dac705460413a901161433a755b
SamuelDinizTenorio <samueldinizcti@outlook.com>
2024-09-07 11:30:01 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+function pesquisar() {
+    // Obtém a seção onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+  
+    // Inicializa uma string vazia para armazenar os resultados formatados
+    let resultados = "";
+  
+    // Itera sobre cada dado na lista de dados
+    for (let dado of dados) {
+      // Cria um novo elemento div para cada resultado
+      resultados += `
+        <div class="item-resultado">
+            <h2>
+                ${dado.titulo} 
+            </h2>
+            <p class="descricao-meta">
+                ${dado.descricao} 
+            </p>
+            <a href=${dado.link} target="_blank">
+                Mais informações
+            </a> 
+        </div>
+      `;
+    }
+  
+    // Atribui o HTML gerado para a seção de resultados
+    section.innerHTML = resultados;
+  }
---
+++ dados.js
@@ -0,0 +1,22 @@
+let rayssa = {
+    titulo : "Rayssa Leal",
+    descricao : "Rayssa Leal: A Fadinha do Skate. Rayssa Leal, carinhosamente apelidada de Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos 16 anos, ela já coleciona diversos títulos, incluindo medalhas olímpicas de prata e bronze, além de ser campeã pan-americana e mundial.",
+    link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+}
+
+let rebeca = {
+    titulo : "Rebeca Andrade",
+    descricao : "Rebeca Andrade: A Maior Medalhista Olímpica Brasileira. Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros com sua determinação e talento. Bicampeã olímpica e a maior medalhista da história do Brasil nos Jogos Olímpicos, Rebeca se tornou um símbolo nacional do esporte.",
+    link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+}
+
+let beatriz = {
+    titulo: "Beatriz Sousa",
+    descricao: "Beatriz Souza: Um Novo Brilho no Judô Brasileiro. Beatriz Souza, a Bia, é uma judoca brasileira que vem conquistando o mundo com suas performances excepcionais. A atleta, que já é considerada um dos principais nomes do judô feminino brasileiro, coleciona medalhas em diversas competições, como o Mundial e o Pan-Americano. Sua força, técnica e determinação a levaram ao topo do pódio em diversas oportunidades, tornando-a um grande orgulho para o Brasil.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+};
+
+let dados = [
+    rayssa, 
+    rebeca, 
+    beatriz];
